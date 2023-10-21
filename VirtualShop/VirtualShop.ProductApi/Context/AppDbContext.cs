d7a854d512d365f6a88929c746c0841bb5acb1cc
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-10-21 10:07:55 -0300

    Criacao do projeto, models e classe de contexto

---
+++ VirtualShop/VirtualShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    // TODO - Usar Fluent API para sobrescrever as convenções do Entity Framework Core
+
+}
---
+++ VirtualShop/VirtualShop.ProductApi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualShop.ProductApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImageURL { get; set; }
+    public int CategoryID { get; set; }
+
+    // Propriedades de navegação, não definidas quando o banco é gerado
+    public Category? Category { get; set; }
+}
