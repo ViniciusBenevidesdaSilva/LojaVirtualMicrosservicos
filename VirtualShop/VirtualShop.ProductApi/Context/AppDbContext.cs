74409d88c8fd031c9f70ba3badeba84f7f19b651
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-10-21 10:45:54 -0300

    Criacao migrations Inicial e SeedProducts

--- VirtualShop/VirtualShop.ProductApi/Context/AppDbContext.cs
+++ VirtualShop/VirtualShop.ProductApi/Context/AppDbContext.cs
@@ -10,6 +10,68 @@ public class AppDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 
-    // TODO - Usar Fluent API para sobrescrever as convenções do Entity Framework Core
+    // Usar Fluent API para sobrescrever as convenções do Entity Framework Core
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        // Category
+        CategoryAttributes(mb);
+
+        // Product
+        ProductAttributes(mb);
+
+
+        // Relationships
+        Relationships(mb);
+
+        // Popular a tabela category
+        CategoryHasData(mb);
+    }
+
+    private void CategoryAttributes(ModelBuilder mb)
+    {
+        mb.Entity<Category>().HasKey(c => c.CategoryID);
+        mb.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+
+    private void CategoryHasData(ModelBuilder mb)
+    {
+        mb.Entity<Category>()
+            .HasData(
+                new Category { CategoryID = 1, Name = "Material Escolar" },
+                new Category { CategoryID = 2, Name = "Acessórios" }
+            );
+    }
+
+    private void ProductAttributes(ModelBuilder mb)
+    {
+        mb.Entity<Product>()
+           .Property(c => c.Name)
+           .HasMaxLength(100)
+           .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.Description)
+            .HasMaxLength(255)
+            .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.ImageURL)
+            .HasMaxLength(255)
+            .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.Price)
+            .HasPrecision(12, 2);
+    }
+
+    private void Relationships(ModelBuilder mb)
+    {
+        mb.Entity<Category>()
+            .HasMany(c => c.Products)
+            .WithOne(p => p.Category)
+            .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+    }
+
 
 }
---
+++ VirtualShop/VirtualShop.ProductApi/Migrations/20231021133524_SeedProducts.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualShop.ProductApi.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "Insert Into Products " +
+                    "(Name, Price, Description, Stock, ImageURL, CategoryID) " +
+                 "Values " +
+                    "('Caderno', 7.55, 'Caderno', 10, 'caderno1.jpg', 1)"                );
+
+            migrationBuilder.Sql(
+                "Insert Into Products " +
+                    "(Name, Price, Description, Stock, ImageURL, CategoryID) " +
+                 "Values " +
+                    "('Lápis', 3.45, 'Lápis Preto', 20, 'lapis1.jpg', 1)"                );
+
+            migrationBuilder.Sql(
+                "Insert Into Products " +
+                    "(Name, Price, Description, Stock, ImageURL, CategoryID) " +
+                 "Values " +
+                    "('Clips', 5.33, 'Clips para papel', 50, 'clips1.jpg', 2) "
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Products");
+        }
+    }
+}
