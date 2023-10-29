abe8db4fdc083689f5bb8405a7cda1b33aeb25c6
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-10-29 13:38:27 -0300

    Criacao Projeto WEB e ProductService

---
+++ VirtualShop.ProductApi/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.DTOs.Mappings;
+
+/// <summary>
+/// Mapeamento que relaciona as entidades com seus respectivos DTOs
+/// </summary>
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+
+        CreateMap<ProductDTO, Product>();
+
+        CreateMap<Product, ProductDTO>()
+            .ForMember(x => x.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+
+    }
+}
---
+++ VirtualShop.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtualShop.Web.Models;
+
+public class CategoryViewModel
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+}
