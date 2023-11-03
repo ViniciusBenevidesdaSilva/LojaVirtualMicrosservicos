4516b11f104f8f4027609b28258bb235caf80628
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-11-03 19:31:31 -0300

    Criação projeto Identity Server

---
+++ VirtualShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace VirtualShop.IdentityServer.Configuration;
+
+public class IdentityConfiguration
+{
+    public const string Admin = "Admin";
+    public const string Clint = "Client";
+
+    public static IEnumerable<IdentityResource> IdentiyResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("vshop", "VShop Server"),
+            new ApiScope(name: "read", "Read data."),
+            new ApiScope(name: "write", "Write data."),
+            new ApiScope(name: "delete", "Delete data.")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                ClientSecrets = {new Secret("123456".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read", "write", "profile"}
+            },
+            new Client
+            {
+                ClientId = "vshop",
+                ClientSecrets = {new Secret("123456".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"https://localhost:7165/signin-oidc"},
+                PostLogoutRedirectUris = { "https://localhost:7165/signout-callback-oidc" },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "vshop"
+                }
+            }
+        };
+
+}
